4ae33a1b2652254533ec165b60c9f65f9ae4dd88
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-06-02 17:58:33 -0400

    temp

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,20 +1,31 @@
 import { useState } from "react";
 import Artists from "./Artists";
 import { useRecoilState } from 'recoil'
+import heart from '../assets/img/heart.png'
 
 import { numOfItems, itemType } from '../assets/atoms'
 import Playlist from "./Playlist";
 
 export default function Content() {
+    const [receivedPlaylists, setReceivedPlaylists] = useState(true);
+
 
 
     return (
         <div className="content">
             <h1>Your Playlists</h1>
-            <div className="playlists">
-                <Playlist />
-                <Playlist />
-            </div>
+
+            {!receivedPlaylists ?
+                <button className="getPlaylists"></button>
+
+
+                : <div className="playlists">
+                    <Playlist coverImage={heart} title="Liked Songs" numOfTracks="521" />
+                    <Playlist />
+                </div>
+
+            }
+
 
         </div>
     )
--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,21 +1,24 @@
-import heart from '../assets/img/heart.png'
-import shuffle from '../assets/img/shuffle.png'
+
+import shuffle from '../assets/img/shuffle.svg'
 import musicNote from '../assets/img/music-note.png'
 
 
-export default function Playlist() {
+export default function Playlist(props) {
     return (
         <div className="playlist">
-            <img src={heart} />
+            <img src={props.coverImage} className='heart' />
             <div className='text'>
-                <h2>Liked Songs</h2>
-                <div className='num-of-songs'>
+                <h2>{props.title}</h2>
+                <div className='songs-stat'>
                     <img className="music-note" src={musicNote}></img>
-                    <h3>521 Tracks</h3>
+                    <h3>{props.numOfTracks} Tracks</h3>
 
                 </div>
 
             </div>
+            <button className='shuffle-btn'>
+                <img src={shuffle}></img>
+            </button>
 
         </div>
     )
