5a75fea5639c940b7468e02e27afaacdef9949fa
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-07-06 22:11:59 -0400

    PlaylistCreator has everything needed in order to create the actual playlists

--- src/components/PlaylistCreator.jsx
+++ src/components/PlaylistCreator.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import { useRecoilState } from 'recoil'
+import { tokenState, genresWithTracksState } from "../assets/atoms"
 
 
 export default function PlaylistCreator() {
+    const [genresWithTracks, setGenresWithTracks] = useRecoilState(genresWithTracksState)
+
+    useEffect(() => {
+        console.log(genresWithTracks)
+    }, [])
     return <></>;
 }
 
