c00171f0c631a898100586c1a2ad1830878f50a0
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-07-07 12:14:28 -0400

    started working on getting playlist creation to work

--- src/components/PlaylistCreator.jsx
+++ src/components/PlaylistCreator.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { errorSelector, useRecoilState } from 'recoil'
 import { tokenState, genresWithTracksState } from "../assets/atoms"
 import { getUserId, createPlaylist } from "../assets/api";
@@ -7,22 +7,33 @@ import { getUserId, createPlaylist } from "../assets/api";
 export default function PlaylistCreator() {
     const [token, setToken] = useRecoilState(tokenState)
     const [genresWithTracks, setGenresWithTracks] = useRecoilState(genresWithTracksState)
-
-
-
+    const [userId, setUserId] = useState("")
+    const [currentPlaylistId, setCurrentPlaylistId] = useState("")
 
     useEffect(() => {
-        getUserId(token).then(userId => {
-            createPlaylist(token, "test1", userId).then(() => {
-                console.log("Playlist Created")
-            }).catch(err => {
-                console.error(`Error creating playlist:${err}`)
-            })
-        }).catch(err => {
-            console.log(err)
-        })
+        getUserId(token)
+            .then(async (userId) => {
+                setUserId(userId);
 
-    }, [])
+                await Promise.all(
+                    genresWithTracks.map(async (track) => {
+                        if (track[3] === true) {
+                            // If the track has been selected by the user
+                            try {
+                                const playlistData = await createPlaylist(token, track[0], userId);
+                                console.log(playlistData);
+                            } catch (err) {
+                                console.log(err);
+                            }
+                        }
+                    })
+                );
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
     return <></>;
 }
 
