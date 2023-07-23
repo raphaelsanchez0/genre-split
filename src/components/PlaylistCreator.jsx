faf31d8008c21387df2f69617399de26a89dac63
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-07-23 15:16:18 -0400

    Revert "user feedback after playlist creation is given in the form of a message explicitly telling them what has been created"
    This reverts commit d14a03e8abeccd0585a975a452b2621cae01c5a8.

--- src/components/PlaylistCreator.jsx
+++ src/components/PlaylistCreator.jsx
@@ -9,39 +9,41 @@ export default function PlaylistCreator() {
     const [userId, setUserId] = useState("")
     const [currentPlaylistId, setCurrentPlaylistId] = useState("")
 
+    const [userPlaylists, setUserPlaylists] = useRecoilState(userPlaylistsState)
+
+
+
     useEffect(() => {
-        getUserId(token)
-            .then(async (userId) => {
-                setUserId(userId);
+        console.log(userPlaylists, "userplaylist");
 
-                await Promise.all(
-                    genresWithTracks.map(async (genre) => {
-                        if (genre[3] === true) {
-                            // If the track has been selected by the user
-                            try {
 
-                                const playlistData = await createPlaylist(token, genre[0], userId);
-                                //creates a new playlist for the genre
-                                const playlistId = playlistData.id;
 
-                                const tracksInGenre = genre[2]
+        (async () => {
+            await Promise.allSettled(
+                genresWithTracks.map(async (genre) => {
+                    if (genre[3] === true) {
+                        // If the track has been selected by the user
 
-                                const URIs = createArrayOfFormatedSpotifyURIs(tracksInGenre);
+                        try {
 
-                                //get all the ids from genre[2]
-                                console.log(URIs)
 
-                            } catch (err) {
-                                console.log(err);
-                            }
+
+                            const playlistData = await createPlaylist(token, genre[0], userId);
+                            // creates a new playlist for the genre
+                            const playlistId = playlistData.id;
+                            const tracksInGenre = genre[2];
+                            const URIs = createArrayOfFormatedSpotifyURIs(tracksInGenre);
+                            // get all the ids from genre[2]
+                            const response = await addToPlaylist(token, playlistId, URIs);
+                            console.log(response, "test");
+                        } catch (err) {
+                            console.error(err);
                         }
-                    })
-                );
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, []);
+                    }
+                })
+            );
+        })();
+    }, [genresWithTracks]);
 
     return <></>;
 }
