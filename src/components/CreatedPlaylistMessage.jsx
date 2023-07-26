c67b8a6854e8e1a19756f6fccfe1d6bdec5fa321
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-07-26 16:02:39 -0400

    made the playlist creator have a button that leadds the user back to the home page

--- src/components/CreatedPlaylistMessage.jsx
+++ src/components/CreatedPlaylistMessage.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-export default function CreatedPlaylistMessage({ message }) { // Destructuring props for cleaner syntax
+export default function CreatedPlaylistMessage({ message: playlist }) { // Destructuring props for cleaner syntax
 
-    const displayMessage = message === "Done"
-        ? "All playlists created!"
-        : `A ${message} playlist has been created!`;
+    const [isDone, setIsDone] = useState(false);
 
-    const style = message === "Done"
+    useEffect(() => {
+        if (playlist === "Done") {
+            setIsDone(true);
+        } else {
+            setIsDone(false);
+        }
+    }, [playlist]);  // This effect runs whenever the `playlist` prop changes
+
+    const displayText = isDone
+        ? "All playlists created!  Start over?"
+        : `A ${playlist} playlist has been created!`;
+
+    const style = isDone
         ? { backgroundColor: '#60f086' }
         : {};
-    return (
+
+    return isDone ? (
+        <Link to="/" className='created-playlist-message' style={style}>
+            <h3 >
+                {displayText}
+            </h3>
+        </Link>
+    ) : (
         <h3 className='created-playlist-message' style={style}>
-            {displayMessage}
+            {displayText}
         </h3>
-    )
+    );
 }
