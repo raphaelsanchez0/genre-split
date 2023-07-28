34747bf047cae18428ddae4d2c50a10927eb64a5
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-07-28 15:32:37 -0400

    fixed ui elementes

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -5,9 +5,11 @@ export default function LoginPage() {
 
     return (
         <div className='login-page'>
-            <h1></h1>
-            <h1>Please Login </h1>
-            <Login />
+            <h1 className='title'>Genrify</h1>
+
+
+            <h2>Sort your playlists by genre with the click of a button</h2>
+            <Login text="Login with Spotify" className="login" />
         </div>
     )
 }
